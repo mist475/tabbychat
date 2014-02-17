df80995e79026cbd248c6a175df3cb7a7e2e19cd
Dykam <kramieb@gmail.com>
2014-02-18 00:55:41 +0100

    Updated TCSettingTextBox.java to MCP9.03 Fixes #16

--- acs/tabbychat/settings/TCSettingTextBox.java
+++ acs/tabbychat/settings/TCSettingTextBox.java
@@ -1,7 +1,7 @@
 package acs.tabbychat.settings;
 
-import net.minecraft.src.Minecraft;
-import net.minecraft.src.GuiTextField;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiTextField;
 
 public class TCSettingTextBox extends TCSetting implements ITCSetting {
 	protected GuiTextField textBox;
@@ -9,94 +9,94 @@ public class TCSettingTextBox extends TCSetting implements ITCSetting {
 	{
 		this.type = "textbox";
 	}
-		
+
 	public TCSettingTextBox(Object theSetting, String theProperty, String theCategory, int theID) {
 		super(theSetting, theProperty, theCategory, theID);
-		this.width = 50;
-		this.height = 11;
-		this.textBox = new GuiTextField(mc.fontRenderer, 0, 0, this.width, this.height);
+		this.width(50);
+		this.height(11);
+		this.textBox = new GuiTextField(mc.fontRenderer, 0, 0, this.width(), this.height());
 		this.textBox.setText((String)this.value);
 	}
-	
+
 	public void clear() {
 		super.clear();
 		this.textBox.setText((String)this.theDefault);
 	}
-	
+
 	public void disable() {
 		super.disable();
-		this.textBox.setEnabled(false);
+		this.textBox.func_146184_c(false);
 	}
-	
+
 	public void drawButton(Minecraft par1, int cursorX, int cursorY) {
 		int labelColor = (this.enabled) ? 0xffffff : 0x666666;
-		
+
 		this.textBox.drawTextBox();
-		this.drawCenteredString(mc.fontRenderer, this.description, this.labelX + mc.fontRenderer.getStringWidth(this.description)/2, this.yPosition + (this.height - 6) / 2,  labelColor);
+		this.drawCenteredString(mc.fontRenderer, this.description, this.labelX + mc.fontRenderer.getStringWidth(this.description)/2, this.y() + (this.height() - 6) / 2,  labelColor);
 	}
-	
+
 	public void enable() {
 		super.enable();
-		this.textBox.setEnabled(true);
+		this.textBox.func_146184_c(true);
 	}
-	
-	public void setEnabled(boolean val) {
+
+	public void func_146184_c(boolean val) {
 		this.enabled = val;
-		this.textBox.setEnabled(val);
+		this.textBox.func_146184_c(val);
 	}
-	
+
 	public String getTempValue() {
 		return this.textBox.getText().trim();
 	}
-	
+
 	public String getValue() {
 		return (String)this.value;
 	}
-	
+
 	public void keyTyped(char par1, int par2) {
 		this.textBox.textboxKeyTyped(par1, par2);
 	}
-	
+
 	public void mouseClicked(int par1, int par2, int par3) {
 		this.textBox.mouseClicked(par1, par2, par3);
 	}
-	
+
 	private void reassignField() {
 		String tmp = this.textBox.getText();
-		this.textBox = new GuiTextField(mc.fontRenderer, this.xPosition, this.yPosition+1, this.width, this.height+1);
-		this.textBox.setMaxStringLength(this.charLimit);
+		this.textBox = new GuiTextField(mc.fontRenderer, this.x(), this.y()+1, this.width(), this.height()+1);
+		this.textBox.func_146203_f(this.charLimit);
 		this.textBox.setText(tmp);
 	}
-	
+
 	public void reset() {
 		if(this.value == null) this.value = "";
 		this.textBox.setText((String)this.value);
 	}
-	
+
 	public void save() {
 		this.value = this.textBox.getText().trim();
 	}
-	
+
 	public void setButtonDims(int wide, int tall) {
 		super.setButtonDims(wide, tall);
 		this.reassignField();
 	}
-	
+
 	public void setButtonLoc(int bx, int by) {
 		super.setButtonLoc(bx, by);
 		this.reassignField();
 	}
-	
+
 	public void setCharLimit(int newLimit) {
 		this.charLimit = newLimit;
-		this.textBox.setMaxStringLength(newLimit);
+		this.textBox.func_146203_f(newLimit);
 	}
-	
+
 	public void setDefault(Object newDefault) {
 		this.theDefault = newDefault;
 	}
-	
+
 	public void setTempValue(Object theVal) {
 		this.textBox.setText((String)theVal);
-	}	
+	}
 }
