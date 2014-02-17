d7ee7a5ecc2081386fa29fdf0e77a75261631b79
Dykam <kramieb@gmail.com>
2014-02-17 23:28:47 +0100

    Updated TCSetting.java to MCP9.03 Fixes #13

--- acs/tabbychat/settings/TCSetting.java
+++ acs/tabbychat/settings/TCSetting.java
@@ -1,13 +1,10 @@
 package acs.tabbychat.settings;
 
-import java.util.Properties;
-
-import org.lwjgl.opengl.GL11;
-
 import acs.tabbychat.core.TabbyChat;
-import net.minecraft.src.Minecraft;
-import net.minecraft.src.FontRenderer;
-import net.minecraft.src.GuiButton;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+
+import java.util.Properties;
 
 abstract class TCSetting extends GuiButton implements ITCSetting {
 	public int buttonColor = 0xbba5e7e4;
@@ -21,7 +18,7 @@ abstract class TCSetting extends GuiButton implements ITCSetting {
 	protected Object tempValue;
 	protected Object theDefault;
 	protected static Minecraft mc = Minecraft.getMinecraft();
-			
+
 	public TCSetting(Object theSetting, String theProperty, String theCategory, int theID) {
 		super(theID, 0, 0, "");
 		this.labelX = 0;
@@ -32,103 +29,135 @@ abstract class TCSetting extends GuiButton implements ITCSetting {
 		this.categoryName = theCategory;
 		this.description = TabbyChat.translator.getString(theCategory + "." + theProperty.toLowerCase());
 	}
-	
+
 	public TCSetting(Object theSetting, String theProperty, String theCategory, int theID, FormatCodeEnum theFormat) {
 		this(theSetting, theProperty, theCategory, theID);
 		this.description = theFormat.toCode() + this.description + "\u00A7r";
 	}
-		
+
 	public void actionPerformed() { }
-	
+
+    public int width() {
+        return this.field_146120_f;
+    }
+
+    public void width(int _w) {
+        this.field_146120_f = _w;
+    }
+
+    public int height() {
+        return this.field_146121_g;
+    }
+
+    public void height(int _h) {
+        this.field_146121_g = _h;
+    }
+
+    public int x() {
+        return field_146128_h;
+    }
+
+    public void x(int _x) {
+        field_146128_h = _x;
+    }
+
+    public int y() {
+        return field_146129_i;
+    }
+
+    public void y(int _y) {
+        field_146129_i = _y;
+    }
+
 	public void clear() {
 		this.value = this.theDefault;
 		this.tempValue = this.theDefault;
 	}
-	
+
 	public void disable() {
 		this.enabled = false;
 	}
-	
+
 	public void drawButton(Minecraft mc, int cursorX, int cursorY) { }
-	
+
 	public void enable() {
 		this.enabled = true;
 	}
-	
+
 	public boolean enabled() {
 		return this.enabled;
 	}
-	
+
 	public Object getDefault() {
 		return this.theDefault;
 	}
-	
+
 	public String getProperty() {
 		return this.propertyName;
 	}
-		
+
 	public Object getTempValue() {
 		return this.tempValue;
 	}
-	
+
 	public String getType() {
 		return this.type;
 	}
-		
+
 	protected Object getValue() {
 		return this.value;
 	}
-		
+
 	public Boolean hovered(int cursorX, int cursorY) {
-		return cursorX >= this.xPosition && cursorY >= this.yPosition && cursorX < this.xPosition + this.width && cursorY < this.yPosition + this.height;
+		return cursorX >= this.x() && cursorY >= this.y() && cursorX < this.x() + this.width() && cursorY < this.y() + this.height();
 	}
-	
+
 	public void loadSelfFromProps(Properties readProps) {
 		this.setCleanValue(readProps.get(this.propertyName));
 	}
-		
+
 	public void mouseClicked(int par1, int par2, int par3) { }
-		
+
 	public void reset() {
 		this.tempValue = this.value;
 	}
-	
+
 	public void resetDescription() {
 		this.description = TabbyChat.translator.getString(this.categoryName + "." + this.propertyName.toLowerCase());
 	}
-	
+
 	public void save() {
 		this.value = this.tempValue;
 	}
-	
+
 	public void saveSelfToProps(Properties writeProps) {
 		if(this.value instanceof Enum) writeProps.put(this.propertyName, ((Enum)this.value).name());
 		else writeProps.put(this.propertyName, this.value.toString());
 	}
-		
+
 	public void setButtonDims(int wide, int tall) {
-		this.width = wide;
-		this.height = tall;
+		this.width( wide);
+		this.height(tall);
 	}
-		
+
 	public void setButtonLoc(int bx, int by) {
-		this.xPosition = bx;
-		this.yPosition = by;
+		this.x(bx);
+		this.y(by);
 	}
-	
+
 	public void setLabelLoc(int _x) {
 		this.labelX = _x;
 	}
-			
+
 	public void setTempValue(Object updateVal) {
 		this.tempValue = updateVal;
 	}
-	
+
 	public void setCleanValue(Object updateVal) {
 		if(updateVal == null) this.clear();
 		else this.value = updateVal;
 	}
-	
+
 	public void setValue(Object updateVal) {
 		this.value = updateVal;
 	}
