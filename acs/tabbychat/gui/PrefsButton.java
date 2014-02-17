a3dd7858e370d566593fda62eebca3d9424fe2f1
Dykam <kramieb@gmail.com>
2014-02-17 23:26:33 +0100

    Updated PrefsButton.java to MCP9.03 Fixes #8

--- acs/tabbychat/gui/PrefsButton.java
+++ acs/tabbychat/gui/PrefsButton.java
@@ -1,53 +1,66 @@
 package acs.tabbychat.gui;
 
-import org.lwjgl.opengl.GL11;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiButton;
 
-import net.minecraft.src.Minecraft;
-import net.minecraft.src.FontRenderer;
-import net.minecraft.src.Gui;
-import net.minecraft.src.GuiButton;
-
-public class PrefsButton extends net.minecraft.src.GuiButton {
+public class PrefsButton extends GuiButton {
 	protected int bgcolor = 0xDD000000;
 	protected boolean hasControlCodes = false;
 	protected String type;
-	
+
 	public PrefsButton() {
 		super(9999, 0, 0, 1, 1, "");
 	}
-	
+
 	public PrefsButton(int _id, int _x, int _y, int _w, int _h, String _title) {
 		super(_id, _x, _y, _w, _h, _title);
 	}
-	
+
 	public PrefsButton(int _id, int _x, int _y, int _w, int _h, String _title, int _bgcolor) {
 		super(_id, _x, _y, _w, _h, _title);
 		this.bgcolor = _bgcolor;
 	}
-	
+
 	protected void title(String newtitle) {
 		this.displayString = newtitle;
 	}
-	
+
 	protected String title() {
 		return this.displayString;
 	}
-	
-	protected int width() {
-		return this.width;
-	}
-	
-	protected void width(int _w) {
-		this.width = _w;
-	}
-	
-	protected int height() {
-		return this.height;
-	}
-	
-	protected void height(int _h) {
-		this.height = _h;
-	}
+
+    public int width() {
+        return this.field_146120_f;
+    }
+
+    public void width(int _w) {
+        this.field_146120_f = _w;
+    }
+
+    public int height() {
+        return this.field_146121_g;
+    }
+
+    public void height(int _h) {
+        this.field_146121_g = _h;
+    }
+
+    public int x() {
+        return field_146128_h;
+    }
+
+    public void x(int _x) {
+        field_146128_h = _x;
+    }
+
+    public int y() {
+        return field_146129_i;
+    }
+
+    public void y(int _y) {
+        field_146129_i = _y;
+    }
 
 	protected int adjustWidthForControlCodes() {
 		String cleaned = this.displayString.replaceAll("(?i)\u00A7[0-9A-FK-OR]", "");
@@ -58,12 +71,12 @@ public class PrefsButton extends net.minecraft.src.GuiButton {
 			goodWidth += cleaned.length();
 		return (badWidth > goodWidth) ? badWidth - goodWidth : 0;
 	}
-	
+
 	public void drawButton(Minecraft mc, int cursorX, int cursorY) {
-	      if(this.drawButton) {
+	      if(this.field_146125_m) {
 	          FontRenderer fr = mc.fontRenderer;
-              drawRect(this.xPosition, this.yPosition, this.xPosition + this.width, this.yPosition + this.height, this.bgcolor);
-	          boolean hovered = cursorX >= this.xPosition && cursorY >= this.yPosition && cursorX < this.xPosition + this.width && cursorY < this.yPosition + this.height;
+              drawRect(this.x(), this.y(), this.x() + this.width(), this.y() + this.height(), this.bgcolor);
+	          boolean hovered = cursorX >= this.x() && cursorY >= this.y() && cursorX < this.x() + this.width() && cursorY < this.y() + this.height();
 
 	          int var7 = 0xa0a0a0;
 	          if(!this.enabled) {
@@ -71,12 +84,12 @@ public class PrefsButton extends net.minecraft.src.GuiButton {
 	          } else if(hovered) {
 	             var7 = 0xffffa0;
 	          }
-	          
+
 	          if (this.hasControlCodes) {
 	        	  int offset = this.adjustWidthForControlCodes();
-	        	  this.drawCenteredString(fr, this.displayString, this.xPosition + (this.width + offset) / 2, this.yPosition + (this.height - 8) / 2, var7);
+	        	  this.drawCenteredString(fr, this.displayString, this.x() + (this.width() + offset) / 2, this.y() + (this.height() - 8) / 2, var7);
 	          } else
-	        	  this.drawCenteredString(fr, this.displayString, this.xPosition + this.width / 2, this.yPosition + (this.height - 8) / 2, var7);
-	       }		
+	        	  this.drawCenteredString(fr, this.displayString, this.x() + this.width() / 2, this.y() + (this.height() - 8) / 2, var7);
+	       }
 	}
 }
