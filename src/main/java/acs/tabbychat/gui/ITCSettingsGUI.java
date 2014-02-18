86af38bb4686719272397584c534550ac84306ad
Dykam <kramieb@gmail.com>
2014-02-18 23:19:48 +0100

    Shifting around

---
+++ src/main/java/acs/tabbychat/gui/ITCSettingsGUI.java
@@ -0,0 +1,49 @@
+package acs.tabbychat.gui;
+
+import java.io.File;
+import java.util.Properties;
+
+import acs.tabbychat.util.TabbyChatUtils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+
+public interface ITCSettingsGUI {
+	int SAVEBUTTON = 8901;
+	int CANCELBUTTON = 8902;
+	int MARGIN = 4;
+	int LINE_HEIGHT = 14;
+	int DISPLAY_WIDTH = 300;
+	int DISPLAY_HEIGHT = 180;
+	File tabbyChatDir = TabbyChatUtils.getTabbyChatDir();
+
+	public void actionPerformed(GuiButton button);
+
+	public void defineDrawableSettings();
+
+	public void drawScreen(int x, int y, float f);
+
+	public void handleMouseInput();
+
+	public void initDrawableSettings();
+
+	public void initGui();
+
+	public void keyTyped(char par1, int par2);
+
+	public Properties loadSettingsFile();
+
+	public void mouseClicked(int par1, int par2, int par3);
+
+	abstract void resetTempVars();
+
+	abstract int rowY(int rowNum);
+
+	abstract void saveSettingsFile();
+
+	abstract void saveSettingsFile(Properties preProps);
+
+	abstract void storeTempVars();
+
+	public void validateButtonStates();
+}
---
+++ src/main/java/acs/tabbychat/settings/ITCSetting.java
@@ -0,0 +1,54 @@
+package acs.tabbychat.settings;
+
+import java.util.Properties;
+
+import net.minecraft.client.Minecraft;
+
+public interface ITCSetting {
+
+	public void actionPerformed();
+
+	public void clear();
+
+	public void disable();
+
+	public void drawButton(Minecraft mc, int cursorX, int cursorY);
+
+	public void enable();
+
+	public boolean enabled();
+
+	public Object getDefault();
+
+	public String getProperty();
+
+	public Object getTempValue();
+
+	public String getType();
+
+	public Boolean hovered(int cursorX, int cursorY);
+
+	public void loadSelfFromProps(Properties readProps);
+
+	public void mouseClicked(int par1, int par2, int par3);
+
+	public void reset();
+
+	public void resetDescription();
+
+	public void save();
+
+	public void saveSelfToProps(Properties writeProps);
+
+	public void setButtonDims(int wide, int tall);
+
+	public void setButtonLoc(int bx, int by);
+
+	public void setLabelLoc(int lx);
+
+	public void setTempValue(Object updateVal);
+
+	public void setCleanValue(Object uncleanVal);
+
+	public void setValue(Object updateVal);
+}
