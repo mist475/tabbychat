c28d961fc61a3971afaf0dc90ef1891c0169b51c
Dykam <kramieb@gmail.com>
2014-02-18 03:08:16 +0100

    Updated TCChatLine.java and TCChatLineFake.java to MCP9.03 Fixes #17 and fixes #18

--- acs/tabbychat/core/TCChatLine.java
+++ acs/tabbychat/core/TCChatLine.java
@@ -1,45 +1,46 @@
 package acs.tabbychat.core;
 
+import acs.tabbychat.util.TCChatLineFake;
+import net.minecraft.client.gui.ChatLine;
+import net.minecraft.util.ChatComponentStyle;
+import net.minecraft.util.IChatComponent;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-import acs.tabbychat.util.TCChatLineFake;
-
-import net.minecraft.src.ChatLine;
-
 public class TCChatLine extends TCChatLineFake implements Serializable {
 	private static final long serialVersionUID = 646162627943686174L;
 	protected boolean statusMsg = false;
 	protected String timeStamp = "";
-		
-	public TCChatLine(int _counter, String _string, int _id) {
+
+	public TCChatLine(int _counter, IChatComponent _string, int _id) {
 		super(_counter, _string, _id);
 	}
-	
+
 	public TCChatLine(ChatLine _cl) {
-		super(_cl.getUpdatedCounter(), _cl.getChatLineString(), _cl.getChatLineID());
+		super(_cl.getUpdatedCounter(), _cl.func_151461_a(), _cl.getChatLineID());
 	}
-	
-	public TCChatLine(int _counter, String _string, int _id, boolean _stat) {
+
+	public TCChatLine(int _counter, IChatComponent _string, int _id, boolean _stat) {
 		this(_counter, _string, _id);
 		this.statusMsg = _stat;
 	}
-	
-	protected void setChatLineString(String newLine) {
+
+	protected void setChatLineString(IChatComponent newLine) {
 		this.lineString = newLine;
 	}
-		
+
 	private void writeObject(ObjectOutputStream _write) throws IOException {
-		_write.writeUTF(this.getChatLineString());
+		_write.writeUTF(ChatComponentStyle.Serializer.func_150696_a(this.getChatLineString()));
 		_write.writeBoolean(this.statusMsg);
 		_write.writeUTF(this.timeStamp);
 	}
-	
+
 	private void readObject(ObjectInputStream _read) throws IOException, ClassNotFoundException {
 		this.updateCounterCreated = -1;
-		this.lineString = _read.readUTF();
+		this.lineString = ChatComponentStyle.Serializer.func_150699_a(_read.readUTF());
 		this.statusMsg = _read.readBoolean();
 		this.timeStamp = _read.readUTF();
 	}
--- acs/tabbychat/util/TCChatLineFake.java
+++ acs/tabbychat/util/TCChatLineFake.java
@@ -1,25 +1,27 @@
 package acs.tabbychat.util;
 
-import net.minecraft.src.ChatLine;
+import net.minecraft.client.gui.ChatLine;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.IChatComponent;
 
 public class TCChatLineFake extends ChatLine {
 	protected int updateCounterCreated;
-	protected String lineString;
+	protected IChatComponent lineString;
 	protected int chatLineID;
-	
+
 	public TCChatLineFake() {
-		super(-1, "", 0);
+		super(-1, new ChatComponentText(""), 0);
 	}
-	
-	public TCChatLineFake(int _counter, String _string, int _id) {
+
+	public TCChatLineFake(int _counter, IChatComponent _string, int _id) {
 		super(_counter, _string, _id);
 		this.updateCounterCreated = _counter;
-		if(_string == null) this.lineString = "";
+		if(_string == null) this.lineString = new ChatComponentText("");
 		else this.lineString = _string;
 		this.chatLineID = _id;
 	}
-	
-	public String getChatLineString() {
+
+	public IChatComponent getChatLineString() {
 		return this.lineString;
 	}
 	public int getUpdatedCounter() {
