1ea009ff5ed41a0305572e130c78d748f8a7c8c7
killjoy1221 <mattmess1221@gmail.com>
2014-02-25 17:59:46 -0500

    Don't interact with Minecraft during the init stage.

--- src/main/java/acs/tabbychat/core/LiteModTabbyChat.java
+++ src/main/java/acs/tabbychat/core/LiteModTabbyChat.java
@@ -41,12 +41,13 @@ public class LiteModTabbyChat implements InitCompleteListener {
 	@Override
 	public void init(File configPath) {
 		String relativeConfig = "tabbychat";
-		File liteConfigDir = LiteLoader.getCommonConfigFolder().toPath()
-				.resolve(relativeConfig).toFile();
-		File mcConfigDir = Minecraft.getMinecraft().mcDataDir.toPath()
-				.resolve("config").resolve(relativeConfig).toFile();
-		
-		// If forge configs exist and liteloader configs don't, copy over.
+		File liteConfigDir = new File(LiteLoader.getCommonConfigFolder(),
+				relativeConfig);
+		File mcConfigDir = new File(LiteLoader.getGameDirectory(),
+				new StringBuilder().append("config").append(File.separatorChar)
+						.append(relativeConfig).toString());
+
+		// If forge old exist and liteloader configs don't, copy over.
 		if (!liteConfigDir.exists() && mcConfigDir.exists()) {
 			try {
 				FileUtils.copyDirectory(mcConfigDir, liteConfigDir);
