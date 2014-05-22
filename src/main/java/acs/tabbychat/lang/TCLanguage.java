31aab47fdb49a9ca6d292e50588e9a53e0116ab3
killjoy1221 <mattmess1221@gmail.com>
2014-05-21 20:00:46 -0400

    All languages converted.

--- src/main/java/acs/tabbychat/lang/TCLanguage.java
+++
@@ -1,8 +0,0 @@
-package acs.tabbychat.lang;
-
-import java.util.Properties;
-
-abstract class TCLanguage {
-	protected static String provides;
-	protected final static Properties defaults = new Properties();
-}
