08a4c041bce7846a7f0416b1e42c461f81c25a2a
Kristian de Lange <dubbelkris18@outlook.com>
2022-08-22 16:47:20 +0200

    revert to old fix for behaviour. When removing LiteLoader support I removed slightly too much. Also removes unneeded debug. This commit won't get its own release as it doesn't add anything actually new

--- src/main/java/acs/tabbychat/proxy/ClientProxy.java
+++ src/main/java/acs/tabbychat/proxy/ClientProxy.java
@@ -7,18 +7,21 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.WorldEvent;
 
 public class ClientProxy extends CommonProxy{
     @Override
     public void load(FMLInitializationEvent event) {
         TabbyChatUtils.startup();
         FMLCommonHandler.instance().bus().register(this);
-        //needed for early loading
-        MinecraftForge.EVENT_BUS.register(this);
         TabbyChat.modLoaded = true;
+    }
+
+    @SubscribeEvent
+    public void postLoad(FMLNetworkEvent.ClientConnectedToServerEvent event) {
+        //ensure the chat gets loaded quickly
+        GuiNewChatTC.getInstance();
     }
 
     @SubscribeEvent
@@ -28,16 +31,6 @@ public class ClientProxy extends CommonProxy{
         }
     }
 
-    /**
-     * Ensures tc gets loaded before logging in, ensuring no messages are lost in the time between joining a world and opening the chat window
-     * @param event {@link WorldEvent.Load} event
-     */
-    @SubscribeEvent
-    public void onWorldLoad(WorldEvent.Load event) {
-        System.out.println("World Load Event should load but no debug?");
-        //Should wake up tabbychat earlier, resulting in no messages being lost due to tc only activating once you open chat instead of on world
-        GuiNewChatTC wakeUp = GuiNewChatTC.getInstance();
-    }
     private boolean onTickInGui(Minecraft minecraft) {
         TabbyChatUtils.chatGuiTick(minecraft);
         return true;
