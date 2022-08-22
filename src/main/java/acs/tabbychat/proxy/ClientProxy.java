fce8b6d95da6cd3799b86b21dc30d57b3cb35383
Kristian de Lange <dubbelkris18@outlook.com>
2022-08-22 16:12:13 +0200

    fix tabbychat not keeping track of messages sent before the chat is opened

--- src/main/java/acs/tabbychat/proxy/ClientProxy.java
+++ src/main/java/acs/tabbychat/proxy/ClientProxy.java
@@ -1,5 +1,6 @@
 package acs.tabbychat.proxy;
 
+import acs.tabbychat.core.GuiNewChatTC;
 import acs.tabbychat.core.TabbyChat;
 import acs.tabbychat.util.TabbyChatUtils;
 import cpw.mods.fml.common.FMLCommonHandler;
@@ -7,12 +8,16 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
 
 public class ClientProxy extends CommonProxy{
     @Override
     public void load(FMLInitializationEvent event) {
         TabbyChatUtils.startup();
         FMLCommonHandler.instance().bus().register(this);
+        //needed for early loading
+        MinecraftForge.EVENT_BUS.register(this);
         TabbyChat.modLoaded = true;
     }
 
@@ -23,6 +28,16 @@ public class ClientProxy extends CommonProxy{
         }
     }
 
+    /**
+     * Ensures tc gets loaded before logging in, ensuring no messages are lost in the time between joining a world and opening the chat window
+     * @param event {@link WorldEvent.Load} event
+     */
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        System.out.println("World Load Event should load but no debug?");
+        //Should wake up tabbychat earlier, resulting in no messages being lost due to tc only activating once you open chat instead of on world
+        GuiNewChatTC wakeUp = GuiNewChatTC.getInstance();
+    }
     private boolean onTickInGui(Minecraft minecraft) {
         TabbyChatUtils.chatGuiTick(minecraft);
         return true;
